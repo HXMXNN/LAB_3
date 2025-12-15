90e7f76397b12a3a4737956e5ea7d8ee4ea43e81
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2025-12-15 12:39:17 +0300

    Added a header for working with files

---
+++ src/file_io.h
@@ -0,0 +1,7 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+void writeArrayToFile(const char *filename, int arr[], int n);
+int readArrayFromFile(const char *filename, int arr[], int max_size);
+
+#endif
