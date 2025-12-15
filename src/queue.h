f7d5e13e648a1fb8a855bed28087b5b00f0b7106
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2025-12-15 12:05:52 +0300

    Added queue implementation header file

---
+++ src/queue.h
@@ -0,0 +1,18 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct {
+    int *data;
+    int front;
+    int rear;
+    int capacity;
+} Queue;
+
+Queue* createQueue(int capacity);
+void enqueue(Queue *q, int value);
+int dequeue(Queue *q);
+void queueToArray(Queue *q, int *arr);
+int queueSize(Queue *q);
+void freeQueue(Queue *q);
+
+#endif
