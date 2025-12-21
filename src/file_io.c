a9eb2a6c40ef9439eba778e67e150c2c1c060448
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2025-12-21 19:59:55 +0300

    New function added

--- src/file_io.c
+++ src/file_io.c
@@ -30,3 +30,60 @@ int readArrayFromFile(const char *filename, int arr[], int max_size) {
     fclose(file);
     return n;
 }
+
+int* readArrayFromFileDynamic(const char *filename, int *size) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Failed to open file for reading");
+        *size = 0;
+        return NULL;
+    }
+
+    int capacity = 100;
+    int *temp = (int*)malloc(capacity * sizeof(int));
+    if (!temp) {
+        fclose(file);
+        *size = 0;
+        return NULL;
+    }
+
+    int count = 0;
+
+    while (fscanf(file, "%d", &temp[count]) == 1) {
+        count++;
+        if (count >= capacity) {
+            capacity *= 2;
+            int *new_temp = (int*)realloc(temp, capacity * sizeof(int));
+            if (!new_temp) {
+                free(temp);
+                fclose(file);
+                *size = 0;
+                return NULL;
+            }
+            temp = new_temp;
+        }
+    }
+
+    fclose(file);
+
+    if (count == 0) {
+        free(temp);
+        *size = 0;
+        return NULL;
+    }
+
+    int *result = (int*)malloc(count * sizeof(int));
+    if (!result) {
+        free(temp);
+        *size = 0;
+        return NULL;
+    }
+
+    for (int i = 0; i < count; i++) {
+        result[i] = temp[i];
+    }
+
+    free(temp);
+    *size = count;
+    return result;
+}
