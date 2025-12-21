89f8f16ffb5938af7554fae0b0e797df718b74d6
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2025-12-21 20:00:28 +0300

    New function added

--- src/file_io.h
+++ src/file_io.h
@@ -3,5 +3,6 @@
 
 void writeArrayToFile(const char *filename, int arr[], int n);
 int readArrayFromFile(const char *filename, int arr[], int max_size);
+int* readArrayFromFileDynamic(const char *filename, int *size);
 
 #endif
