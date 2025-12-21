b758d506f0159f62dbe2314a55f6f7ab41df6f31
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2025-12-21 20:01:52 +0300

    Removed array

--- src/sorts.h
+++ src/sorts.h
@@ -1,9 +1,9 @@
 #ifndef SORTS_H
 #define SORTS_H
 
-void selectionSort(int arr[], int n);
-void quickSort(int arr[], int first, int last);
+void selectionSort(int *arr, int n);
+void quickSort(int *arr, int first, int last);
 void swap(int *a, int *b);
-void compareSorts(int arr[], int n);
+void compareSorts(int *arr, int n);
 
 #endif
