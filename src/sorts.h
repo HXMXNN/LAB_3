b8211fcd362c32a874f3b23d29e73a27530e8fed
HXMXNN <mxpantsa@gmail.com>
2025-12-29 20:50:37 +0300

    Delete src/sorts.h

--- src/sorts.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORTS_H
-#define SORTS_H
-
-void selectionSort(int *arr, int n);
-void quickSort(int *arr, int first, int last);
-void swap(int *a, int *b);
-void compareSorts(int *arr, int n);
-
-#endif
