463e334a9ce2cdab51db860d857b77b0f388f138
HXMXNN <mxpantsa@gmail.com>
2025-12-29 20:49:53 +0300

    Delete src/file_io.h

--- src/file_io.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
-
-void writeArrayToFile(const char *filename, int arr[], int n);
-int readArrayFromFile(const char *filename, int arr[], int max_size);
-int* readArrayFromFileDynamic(const char *filename, int *size);
-
-#endif
